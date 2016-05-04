fc99192367712eefc6175990822695741e894393
MarRyb <marryb@MacBook-Pro-MarRyb.local>
2016-05-04 00:10:00 -0700

    tests for commit model and finish testing importer libriary

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,18 +1,22 @@
 require 'digest/sha1'
 
 FactoryGirl.define do
-	# sequence(:email) { |n| "user#{n}@mail.com" }
-	# sequence(:name) { |n| "User #{n}"  }
+	sequence(:email) { |n| "user#{n}@mail.com" }
+	sequence(:name) { |n| "User #{n}"  }
 	sequence(:sha) { |n| Digest::SHA1.hexdigest n.to_s }
-	sequence(:date) { |n| DateTime.now - n.days }
+	sequence(:date) { DateTime.now - (1..10).to_a.sample.days }
 	sequence(:message) { |n| "Message ##{n}" }
 
 	factory :user do
-		name "MarRyb"
-		email "marryb@macbook-pro-marryb.local"
+		name
+		email
 
 		factory :user_with_commits do
 			commits { Array.new(10) { build(:commit) } }
+		end
+
+		factory :user_with_several_commits do
+			commits { Array.new(2) { build(:commit) } }
 		end
 
 	end
--- spec/models/commit_spec.rb
+++ spec/models/commit_spec.rb
@@ -8,6 +8,23 @@ describe Commit do
 	it { should validate_presence_of :user }
 	it { should validate_presence_of :date }
 
-	
+	it "should return only one person commits" do
+		user1 = create :user_with_commits
+		user2 = create :user_with_commits
+
+		result = Commit.by_user_email(user1.email)
+		user_ids = result.map(&:user_id).uniq
+
+		expect(result.count).to eq 10
+		expect(user_ids).to eq [user1.id]
+	end
+
+	it "should order commits per commit date" do
+		user = create :user_with_several_commits
+
+		result = Commit.ordered
+		
+		expect(result[0].date).to be > result[1].date
+	end
 
 end
